e0b6fdc1daf884148702a14c5546fd4d7e444334
Tolulopoe <treasureddammie@gmail.com>
2023-02-22 20:11:01 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for presence of alphabet
+ * 4-isalpha
+ * @a: char to be checked
+ * Return: 1 (Success) 0(Failure)
+ */
+
+int _isalpha(int a)
+{
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
