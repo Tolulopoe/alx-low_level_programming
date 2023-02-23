3e40b70456b7e424d22f9b3527348025a8ad343e
Tolulopoe <treasureddammie@gmail.com>
2023-02-22 20:17:22 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - To print the absolute value  of a number
+ * 6-abs
+ * @i:  Value to be checked
+ * Return: n if positive, -n if negative
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
