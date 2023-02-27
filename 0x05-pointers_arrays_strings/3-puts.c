690b4af09da004e4825f9916e13eceabd304910e
Tolulopoe <120282656+Tolulopoe@users.noreply.github.com>
2023-02-27 11:32:30 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-#include "holberton.h"
 
-/**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-
-	_putchar('\n');
-}
-
-=======
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -36,23 +16,3 @@ void _puts(char *str)
 
 	_putchar('\n');
 }
-
-=======
-#include "main.h"
-
-/**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-
-	_putchar('\n');
-}
->>>>>>> 311c91d596b5fac2e8aaab4cb24dda9d5037159d
