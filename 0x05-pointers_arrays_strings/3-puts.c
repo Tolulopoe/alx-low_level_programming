ab2e649c649e709f6210f5c5b44dd6e074325538
Tolulopoe <treasureddammie@gmail.com>
2023-02-28 01:03:17 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-#include "holberton.h"
-
-/**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-
-	_putchar('\n');
-}
-
-=======
-<<<<<<< HEAD
 #include "main.h"
-
 /**
   * _puts - Prints a string
   * @str: The string to print
@@ -33,26 +11,6 @@ void _puts(char *str)
 	{
 		_putchar(*str);
 	}
-
 	_putchar('\n');
 }
 
-=======
-#include "main.h"
-
-/**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
-void _puts(char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-
-	_putchar('\n');
-}
->>>>>>> 311c91d596b5fac2e8aaab4cb24dda9d5037159d
